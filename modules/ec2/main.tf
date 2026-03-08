1ab272a05a2c206f1ead80a355db6afaddcb5e34
sunny <sunny@gmail.com>
2026-03-08 16:28:32 +0530

    file

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,13 +5,9 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = [var.sg_id]
   key_name               = var.key_name
 
-  user_data = var.user_data_file != "" ? (
-    var.master_ip != "" ?
-    templatefile(var.user_data_file, {
-      master_ip = var.master_ip
-    }) :
-    file(var.user_data_file)
-  ) : null
+  user_data = var.user_data_file != "" ? templatefile(var.user_data_file, {
+  master_ip = var.master_ip
+}) : null
 
   root_block_device {
     volume_size = 20 # GB
